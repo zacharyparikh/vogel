7887478662d6eb667ce65adbe162e9f14563f735
Zachary Parikh <parikhzachary@gmail.com>
2023-04-23 12:01:01 -0400

    Posts Router

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,16 +1,41 @@
-import { z } from "zod";
-
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { clerkClient } from '@clerk/nextjs/server';
+import { TRPCError } from '@trpc/server';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
 
 export const postsRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({ take: 100 });
+
+    const users = Object.fromEntries(
+      (
+        await clerkClient.users.getUserList({
+          userId: posts.map((post) => post.authorId),
+          limit: 100,
+        })
+      )
+        .flatMap((user) => {
+          const { id, username, profileImageUrl } = user;
+
+          if (!username) {
+            return [];
+          }
+
+          return [{ id, username, profileImageUrl }];
+        })
+        .map((user) => [user.id, user])
+    );
+
+    return posts.map((post) => {
+      const author = users[post.authorId];
+
+      if (!author) {
+        throw new TRPCError({
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Author for Post not found',
+        });
+      }
+
+      return { post, author };
+    });
   }),
 });
